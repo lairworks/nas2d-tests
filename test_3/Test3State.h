7f53ebe19d59a2cb597362682ba4bcd2d3aae2d7
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-03-12 16:44:27 -0600

    Remove tab alignment in Test3State.h

--- test_3/Test3State.h
+++ test_3/Test3State.h
@@ -15,25 +15,25 @@ protected:
 
 private:
 
-	Timer		mCausticsTimer;		/**< Caustics timer. */
+	Timer mCausticsTimer; /**< Caustics timer. */
 
-	FpsCounter	mFps;				/**< FPS Counter */
+	FpsCounter mFps; /**< FPS Counter */
 
-	Font		mFont;				/**< Main font. */
-	Font		mSmallFont;			/**< FPS font. */
+	Font mFont; /**< Main font. */
+	Font mSmallFont; /**< FPS font. */
 
-	Image		mMud;				/**< Background image. */
-	Image		mCaustics;			/**< Caustics image atlas. */
-	Image		mCaustics2;			/**< Caustics image atlas with no alpha. */
+	Image mMud; /**< Background image. */
+	Image mCaustics; /**< Caustics image atlas. */
+	Image mCaustics2; /**< Caustics image atlas with no alpha. */
 
-	Image		mCaustics3;			/**< Caustics image atlas. */
-	Image		mCaustics4;			/**< Caustics image atlas with no alpha. */
+	Image mCaustics3; /**< Caustics image atlas. */
+	Image mCaustics4; /**< Caustics image atlas with no alpha. */
 
-	bool		mAlpha;
-	bool		mMultiply;			/**< Flag to indicate that we want to draw screen blend mode. */
-	bool		mCausticsOnly;		/**< Flag to indicate that we only want to show the caustics. */
+	bool mAlpha;
+	bool mMultiply; /**< Flag to indicate that we want to draw screen blend mode. */
+	bool mCausticsOnly; /**< Flag to indicate that we only want to show the caustics. */
 
-	bool		mAlpha2;
-	bool		mMultiply2;
-	bool		mCausticsOnly2;
+	bool mAlpha2;
+	bool mMultiply2;
+	bool mCausticsOnly2;
 };
