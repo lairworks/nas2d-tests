8ea678f654fed3c6f049c8810c3d648c4341724d
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-07-20 23:36:57 -0600

    Replace Renderer.width()/height() with size().x/.y in Test3State

--- SampleProjects/BlendingModes/Test3State.cpp
+++ SampleProjects/BlendingModes/Test3State.cpp
@@ -69,8 +69,8 @@ NAS2D::State* Test3State::update()
 
 	r.drawText(mFont, "Blending Demo", NAS2D::Point{5, 5}, NAS2D::Color::White);
 	r.drawText(mSmallFont, "Press keys 1 - 6 to change blend modes.", NAS2D::Point{5, 5 + mFont.height()}, NAS2D::Color::White);
-	r.drawText(mSmallFont, mBlendingModeText, NAS2D::Point{r.width() - mSmallFont.width(mBlendingModeText) - 5, 5}, NAS2D::Color::White);
-	r.drawText(mSmallFont, "FPS: " + std::to_string(mFps.fps()), NAS2D::Point{r.width() - 65, r.height() - mSmallFont.height() - 5}, NAS2D::Color::White);
+	r.drawText(mSmallFont, mBlendingModeText, NAS2D::Point{r.size().x - mSmallFont.width(mBlendingModeText) - 5, 5}, NAS2D::Color::White);
+	r.drawText(mSmallFont, "FPS: " + std::to_string(mFps.fps()), NAS2D::Point{r.size().x - 65, r.size().y - mSmallFont.height() - 5}, NAS2D::Color::White);
 
 	return this;
 }
